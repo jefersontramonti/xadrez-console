6b483b261726ce94c495815ce21269d14516fd24
Jeferson Tramonti <jefersontramonti@outlook.com>
2021-03-30 15:12:16 -0300

    melhorando aimplementação do metodo imprimir

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,6 +1,7 @@
 ﻿using tabuleiro;
 using System;
 
+
 namespace xadrez_console
 {
     class Tela
@@ -10,6 +11,7 @@ namespace xadrez_console
 
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -18,12 +20,30 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Tela.imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
 
+            Console.WriteLine("  A B C D E F G H");
+
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
 
         internal static void imprimirTabuleiro(object tab)
